5c0f199e90084096ba439adc9711efeb2012bf38
lesbianboy <52571621+lesbianboy@users.noreply.github.com>
2022-06-12 18:58:45 +0600

    upd0.3

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -3,8 +3,6 @@ public class PlayerAttack : MonoBehaviour
 {
     private float timeBtwAttack;
     public float startTimeBtwAttack;
-
-    public Transform attackPos;
     public LayerMask enemyLayerMask;
     public float attackRange;
     public int damageAmount;
@@ -29,14 +27,5 @@ public class PlayerAttack : MonoBehaviour
         // {
         //     timeBtwAttack -= Time.deltaTime;
         // }
-    }
-    
-    public void OnAttack()
-    {
-        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayerMask);
-        foreach (Collider2D enemy in enemies)
-        {
-            //enemy.GetComponent<Enemy>().TakeDamage(damageAmount);
-        }
     }
 }
